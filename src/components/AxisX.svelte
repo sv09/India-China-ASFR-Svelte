661b79b8c1c272bb807ae41d126068e834eab59f
sv09 <shreyavinod09@gmail.com>
2023-07-10 21:08:51 -0400

    component to render bottom axis

---
+++ src/components/AxisX.svelte
@@ -0,0 +1,15 @@
+<script>
+    export let margin;
+    export let height;
+    export let xVal;
+    export let xScale;
+</script>
+
+<g transform="translate({margin.left}, 0)">
+    {#each xVal as val}
+        <text 
+            x={xScale(val)} 
+            y={height-margin.bottom}> {val}
+        </text>
+    {/each}
+</g>
