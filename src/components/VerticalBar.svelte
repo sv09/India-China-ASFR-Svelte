ea8addd09e8e991e83a53a62c137bdc271515cf3
sv09 <shreyavinod09@gmail.com>
2023-07-10 21:09:42 -0400

    component to render vertical bar chart

---
+++ src/components/VerticalBar.svelte
@@ -0,0 +1,20 @@
+<script>
+    export let data;
+    export let yScale;
+    export let xScale;
+    export let margin;
+    export let height;
+    export let year;
+
+    let plotData = data.filter(d => d.Time === year);
+</script>
+
+<g transform="translate({margin.left}, {-margin.bottom})">
+    {#each plotData as d}
+        <rect x={xScale(d.AgeGrp)} y={yScale(d.ASFR)} width={xScale.bandwidth()-2} height={ height-yScale(d.ASFR)-margin.bottom } fill="rgb(119, 95, 160)"></rect>
+    {/each}  
+</g>
+
+<style>
+
+</style>
