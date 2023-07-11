4528e71e8467b0a0ba3281429dafe451f2595617
sv09 <shreyavinod09@gmail.com>
2023-07-10 21:09:17 -0400

    component to render axis on the left

---
+++ src/components/AxisY.svelte
@@ -0,0 +1,19 @@
+<script>    
+    export let yVal;
+    export let margin;
+    export let yScale;
+
+    let yTickVal = [];
+    for(let i=Math.ceil(yVal[0])*10; i<Math.ceil(yVal[1]); i+=10){
+        yTickVal.push(i);
+    } 
+</script>
+
+<g>
+    {#each yTickVal  as val}
+        <text 
+            x={margin.left} 
+            y={yScale(val)}> {val} 
+        </text>
+    {/each}
+</g>
